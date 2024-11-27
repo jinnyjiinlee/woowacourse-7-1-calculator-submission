71910d2e3a73e3ce45cd93dda31e9deb371a5561
Jinny <ggab.challenge@gmail.com>
2024-11-27 16:28:30 +0900

    refactor: retrun 문 뒤 await 생략

--- src/View/inputView.js
+++ src/View/inputView.js
@@ -3,6 +3,7 @@ import { INPUT_MESSAGES } from '../Constant/messages.js';
 
 export class InputHandler {
   async getStringToAddInput() {
-    return await Console.readLineAsync(INPUT_MESSAGES.STRING_TO_ADD);
+    // TODO: 공부 - return 뒤에 await 없어도 실행 가능한 이유
+    return Console.readLineAsync(INPUT_MESSAGES.STRING_TO_ADD);
   }
 }
